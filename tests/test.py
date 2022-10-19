61f350b3093f715a48a781e063f9dd4f42feb27c
timothy adigun <adigun.timothy@gmail.com>
2022-10-19 05:58:54 +0100

    modified tests/test file

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 import unittest
-import sys 
+import sys
 sys.path.append("..")
 from assertions import assert_eq, asserts
 
 
 class Tester(unittest.TestCase):
     def test_asserts(self):
-        self.assertEqual(asserts([], True))
+        self.assertEqual(asserts([]), False)
 
-    def test_asserts(self):
+    def test_asserts2(self):
         self.assertEqual(asserts("Lagos"), True)
 
     def test_assert_eq(self):
         self.assertEqual(assert_eq([0, 1, 2], list(range(5))), True)
+
+    def test_assert_eq2(self):
+        self.assertEqual(assert_eq([0,1,2,3,4], list(range(5))), True)

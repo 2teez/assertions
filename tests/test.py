98b60cab025c595a24379549f56dcb95b56d2ac6
timothy adigun <adigun.timothy@gmail.com>
2022-10-20 06:28:53 +0100

    modified test.py file

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,16 @@
 import unittest
 import sys
 sys.path.append("..")
-from assertions import assert_eq, asserts
+from assertions import assert_eq, assert_eq_prnt, asserts
 
+def product(lst: list[int]) -> int:
+    result = 1
+    for n in lst:
+        result *= n
+    return result
+
+assert_eq_prnt(product([2,3,5,7]), 21, show=True)
+        
 
 class Tester(unittest.TestCase):
     def test_asserts(self):
@@ -17,3 +25,6 @@ class Tester(unittest.TestCase):
 
     def test_assert_eq2(self):
         self.assertEqual(assert_eq([0,1,2,3,4], list(range(5))), True)
+
+    def test_assert_eq_product(self):
+        self.assertEqual(assert_eq(product([2,3,5,7]), 210), True)

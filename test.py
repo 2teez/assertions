b85a929731205ff117836de28d5bd93916e90ac0
timothy adigun <adigun.timothy@gmail.com>
2022-10-20 06:25:09 +0100

    modified file test.py

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-import assertions
+from assertions import asserts, asserts_prnt, assert_eq, assert_eq_prnt, _print_result
 
 def main() -> None:
-    print(assertions.asserts([]))
-    print(assertions.assert_eq([1, 3], [1, 3]))
-    print(assertions.assert_eq((), list(range(5))))
-    assertions.assert_eq_prnt("tim", "Tim", show=True)
-    assertions._print_result(False, values=[23, list(range(5))], show=True)
+    print(asserts([]))
+    print(assert_eq([1, 3], [1, 3]))
+    print(assert_eq((), list(range(5))))
+    asserts_prnt(0)
+    assert_eq_prnt("tim", "Tim", show=True)
+    _print_result(False, values=[23, list(range(5))], show=True)
 
 
 if __name__ == '__main__':
